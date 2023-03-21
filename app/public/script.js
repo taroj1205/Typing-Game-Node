fc63819377520deeb45aa00213dc819ca6b37601
taroj1205 <taroj1205@gmail.com>
2023-03-21 17:31:29 +1300

    hopefully shifts #link up when keyboard open

--- app/public/script.js
+++ app/public/script.js
@@ -55,6 +55,16 @@ const setVisualViewport = () => {
     const body = document.body;
     body.style.setProperty('--vvw', `${vv.width}px`);
     body.style.setProperty('--vvh', `${vv.height}px`);
+
+    const linkHeight = linkText.clientHeight;
+    const windowHeight = vv.height;
+    const keyboardHeight = windowHeight - vv.innerHeight;
+
+    if (keyboardHeight > 0) {
+        linkText.style.bottom = `${keyboardHeight + linkHeight}px`;
+    } else {
+        linkText.style.bottom = `1rem`;
+    }
 }
 setVisualViewport();
 window.visualViewport.addEventListener('resize', setVisualViewport)
