55ef6299fdf21af70e23c5d5afd26267864e918a
taroj1205 <taroj1205@gmail.com>
2023-03-21 17:08:42 +1300

    #15 Improved design for phone

--- app/public/script.js
+++ app/public/script.js
@@ -52,9 +52,9 @@ window.onload = () => {
 
 const setVisualViewport = () => {
     const vv = window.visualViewport;
-    const root = document.documentElement;
-    root.style.setProperty('--vvw', `${vv.width}px`);
-    root.style.setProperty('--vvh', `${vv.height}px`);
+    const body = document.body;
+    body.style.setProperty('--vvw', `${vv.width}px`);
+    body.style.setProperty('--vvh', `${vv.height}px`);
 }
 setVisualViewport()
 window.visualViewport.addEventListener('resize', setVisualViewport)
