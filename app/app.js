b54894caf29dac5efc264b534612db5561d1582e
taroj1205 <taroj1205@gmail.com>
2023-04-14 15:11:43 +1200

    #30 fix to leaderboard order

--- app/app.js
+++ app/app.js
@@ -418,7 +418,7 @@ app.get('/leaderboard', async (req, res) => {
                 });
             }
 
-            leaderboard.sort((a, b) => b.playtime - a.playtime);
+            leaderboard.sort((a, b) => b.playtime.localeCompare(a.playtime));
 
             const templatePath = path.join(__dirname, 'public', 'html', 'leaderboard', 'index.html');
             const templateSource = fs.readFileSync(templatePath, 'utf8');
